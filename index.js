a2e8bbfe9439569f79f438b6cb480561b82d5594
felipe <felipemedina7878@gmail.com>
2020-12-24 20:52:26 -0500

    end

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", e => validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
   document.body.addEventListener("click", e => closeModal(e));
+  document.body.addEventListener("keyup", e => listenforesc(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -21,8 +22,29 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch(newValue){
+    case-270:
+    document.querySelector('.project1').setAttribute("tabindex",'-1')
+    document.querySelector('project1-container').setAttribute("aria-hidden", "true");
+    document.querySelector('.project4').removeAttribute("tabindex")
+    document.querySelector('project4-container').removeAttribute("aria-hidden");
+    break;
+    case -540:
+      document.querySelector('.project2').setAttribute("tabindex",'-1' )
+      document.querySelector('project2-container').setAttribute("aria-hidden","true");
+      document.querySelector('.project5').removeAttribute("tabindex")
+      document.querySelector('project5-container').removeAttribute("aria-hidden");
+      break;
+  }
 }
 
+function listenforesc(e)
+{
+  if(e.keyCode === 27)
+  {
+    closeModal(e);
+  }
+}
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
 function clickLeft() {
   const currentLeft = parseInt(
@@ -34,11 +56,37 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch(newValue){
+    case-270:
+    document.querySelector('.project5').setAttribute("tabindex","-1" )
+    document.querySelector('.project5-container').setAttribute("aria-hidden","true")
+    document.querySelector('.project2').removeAttribute("tabindex")
+    document.querySelector('.project2-container').removeAttribute("aria-hidden");
+    break;
+    case 0:
+      document.querySelector('.project4').setAttribute("tabindex","-1" )
+      document.querySelector('.project4-container').setAttribute("aria-hidden","true")
+      document.querySelector('.project1').removeAttribute("tabindex")
+      document.querySelector('.project1-container').removeAttribute("aria-hidden")
+      break;
+  }
+}
+function validateForm(e) {
+  e.preventDefault();
+  const nameField = document.getElementById("name");
+  if(nameField.value === ""){
+    document.getElementById("name-error").innerHTML= '!Para enviar el formulario necesita el nombre.'
+  }else{
+    showNotification();
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById("name-error").innerHTML = '';
+  document.querySelector('.form-container').reset();
   document.querySelector(".notification").style.display = "flex";
+  document.querySelector('.notification').innerHTML = 'El formalurio fue enviado sin problemas tkm';
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
@@ -47,11 +95,14 @@ function showNotification() {
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
+  document.querySelector(".modal-project-image").setAttribute("src", e.target.getAttribute("src"))
+  document.getElementById('modal-header').focus();
+  
 }
 
 /** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
-  // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
+  // si el click ocurrio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
   if (
     e.target.className.includes("project") ||
     e.target.className === "modal"
